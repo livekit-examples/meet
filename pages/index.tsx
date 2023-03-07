a30180c966fb00ebcabc413a96893e8237b9c6a1
lukasIO <mail@lukasseiler.de>
2023-03-07 16:46:12 +0100

    support for custom connection details

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,82 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import React, { ReactElement, ReactNode } from 'react';
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+function Tabs(props: { children: ReactElement[] }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  if (!props.children) {
+    return <></>;
+  }
+
+  let tabs = React.Children.map(props.children, (child, index) => {
+    return (
+      <button
+        onClick={() => setActiveIndex(index)}
+        className={activeIndex === index ? 'lk-button tab-button active' : 'lk-button tab-button'}
+      >
+        {child?.props.label}
+      </button>
+    );
+  });
+  return (
+    <div>
+      <div>{tabs}</div>
+      {props.children[activeIndex]}
+    </div>
+  );
+}
+
+function DemoMeetingTab({ label }: { label: string }) {
   const router = useRouter();
   const startMeeting = () => {
     router.push(`/rooms/${generateRoomId()}`);
   };
+  return (
+    <button
+      style={{ fontSize: '1.25rem', paddingInline: '1.25rem' }}
+      className="lk-button"
+      onClick={startMeeting}
+    >
+      Start Meeting
+    </button>
+  );
+}
 
+function CustomConnectionTab({ label }: { label: string }) {
+  const [liveKitUrl, setLiveKitUrl] = useState<string | undefined>();
+  const [token, setToken] = useState<string | undefined>();
+
+  const router = useRouter();
+  const join = () => {
+    router.push(`/custom/?liveKitUrl=${liveKitUrl}&token=${token}`);
+  };
+  return (
+    <div>
+      <label>
+        LiveKit URL
+        <input type="url" onChange={(ev) => setLiveKitUrl(ev.target.value)}></input>
+      </label>
+      <label>
+        Token
+        <input type="text" onChange={(ev) => setToken(ev.target.value)}></input>
+      </label>
+      <button
+        style={{ fontSize: '1.25rem', paddingInline: '1.25rem' }}
+        className="lk-button"
+        onClick={join}
+      >
+        Connect
+      </button>
+    </div>
+  );
+}
+
+const Home: NextPage = () => {
   return (
     <>
-      <main className={styles.main}>
+      <main className={styles.main} data-lk-theme="default">
         <div className="header">
           <img src="/images/livekit-meet-home.svg" alt="LiveKit Meet" width="480" height="60" />
           <h2>
@@ -18,13 +84,10 @@ const Home: NextPage = () => {
             and Next.js.
           </h2>
         </div>
-        <button
-          style={{ fontSize: '1.25rem', paddingInline: '1.25rem' }}
-          className="lk-button"
-          onClick={startMeeting}
-        >
-          Start Meeting
-        </button>
+        <Tabs>
+          <DemoMeetingTab label="Demo" />
+          <CustomConnectionTab label="Custom" />
+        </Tabs>
       </main>
       <footer>
         Hosted on{' '}
