4d4dcabb7d5dbf4b340350d089b7ec19016e9240
Jonas Schell <jonas@livekit.io>
2023-03-30 11:39:25 +0200

    on leave go back to home page

--- pages/rooms/[name].tsx
+++ pages/rooms/[name].tsx
@@ -32,7 +32,10 @@ const Home: NextPage = () => {
           <ActiveRoom
             roomName={roomName}
             userChoices={preJoinChoices}
-            onLeave={() => setPreJoinChoices(undefined)}
+            onLeave={() => {
+              setPreJoinChoices(undefined);
+              router.push('/');
+            }}
           ></ActiveRoom>
         ) : (
           <div style={{ display: 'grid', placeItems: 'center', height: '100%' }}>
