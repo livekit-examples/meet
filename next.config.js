7352ad681f1ed46e947c09a9dbb8d4fd489bb7e0
Jonas Schell <jonas@livekit.io>
2023-04-21 11:06:39 +0200

    add source-map-loader

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
   productionBrowserSourceMaps: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    // Important: return the modified config
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.mjs$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig;
