625498198052a9bb84c633fe17d4124d28ce704d
Jonas Schell <jonas@livekit.io>
2023-04-21 11:19:55 +0200

    turn on source maps for production (#59)
    * turn on source maps
    
    * add source-map-loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
+  swcMinify: false,
+  productionBrowserSourceMaps: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    // Important: return the modified config
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.mjs$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig;
