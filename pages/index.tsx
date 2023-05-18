a779b026de5e06a68bb248ef576c721ca497bb9f
Hugo Arregui <hugo@decentraland.org>
2023-05-18 13:38:21 -0300

    Save Thu May 18 01:38:21 PM -03 2023

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,8 @@ import { useMetaMask } from "metamask-react";
 import { flatFetch } from '../lib/flat-fetch'
 import { signedFetch, loginUsingEthereumProvider } from '../lib/auth'
 
-async function livekitConnect(provider: any) {
+async function livekitConnect(provider: any, worldServer: string, worldName: string) {
     const identity = await loginUsingEthereumProvider(provider)
-    const worldServer = 'https://worlds-content-server.decentraland.zone'
-    const worldName = 'hugoarregui.dcl.eth'
     const aboutResponse = await flatFetch(`${worldServer}/world/${worldName}/about`, {
         responseBodyType: 'json'
     })
@@ -25,19 +23,59 @@ async function livekitConnect(provider: any) {
     return new URL(connStr.replace('livekit:', ''))
 }
 
-const Home = () => {
+function JoinScreen(provider: any) {
     const router = useRouter();
+    const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
 
-    const { status, connect, ethereum } = useMetaMask();
-    if (status === "connected") {
-        livekitConnect(ethereum).then((adapter) => {
+        const formData = new FormData(event.target as HTMLFormElement);
+        const worldServer = formData.get('worldServer')?.toString();
+        const worldName = formData.get('worldName')?.toString();
+
+        livekitConnect(provider, worldServer!, worldName!).then((adapter) => {
             const livekitUrl = adapter.origin
             const token = adapter.searchParams.get('access_token')
             router.push(`/custom?liveKitUrl=${livekitUrl}&token=${token}`)
         })
-        return
-    }
+    };
 
+    return (
+        <form className={styles.tabContent} onSubmit={onSubmit}>
+            <p style={{ marginTop: 0 }}>
+                Connect LiveKit Meet with a custom server using LiveKit Cloud or LiveKit Server.
+            </p>
+            <input
+                id="worldServer"
+                name="worldServer"
+                type="url"
+                placeholder="World Server URL: https://worlds-content-server.decentraland.zone"
+                required
+            />
+            <input
+                id="worldName"
+                name="worldName"
+                type="string"
+                placeholder="World Name: <name>.dcl.eth"
+                required
+            />
+            <hr
+                style={{ width: '100%', borderColor: 'rgba(255, 255, 255, 0.15)', marginBlock: '1rem' }}
+            />
+            <button
+                style={{ paddingInline: '1.25rem', width: '100%' }}
+                className="lk-button"
+                type="submit"
+            >
+                Connect
+            </button>
+        </form>
+    );
+}
+
+
+
+const Home = () => {
+    const { status, connect, ethereum } = useMetaMask();
   return (
     <>
       <main className={styles.main} data-lk-theme="default">
@@ -56,6 +94,9 @@ const Home = () => {
       }
       {
           (status === "connecting") && (<div>Connecting...</div>)
+      }
+      {
+          (status === "connected") && JoinScreen(ethereum)
       }
     </div>
       </main>
