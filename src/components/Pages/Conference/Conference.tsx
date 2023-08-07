db8a2ee4710b1b26c0d76a12f4a84de534e4bbe6
jmoguilevsky <jmoguilevsky@gmail.com>
2023-08-07 17:26:40 -0300

    add styling

--- src/components/Pages/Conference/Conference.tsx
+++ src/components/Pages/Conference/Conference.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { Props } from "./Conference.types"
 import { LiveKitRoom, VideoConference } from "@livekit/components-react"
+import "@livekit/components-styles"
+import "./Conference.css"
 
 export default function Conference(props: Props) {
   const { token, server } = props
 
   return (
     <>
-      <LiveKitRoom token={token} serverUrl={server} connect={true}>
+      <LiveKitRoom token={token} serverUrl={server} connect={true} data-lk-theme="default">
         <VideoConference />
       </LiveKitRoom>
     </>
