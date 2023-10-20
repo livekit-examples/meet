bc63d40431e565de265e3fd58dd4e3393a063801
ocupe <jonas@livekit.io>
2023-10-20 15:32:59 +0200

    add local storage module

---
+++ lib/local-storage.ts
@@ -0,0 +1,23 @@
+import { LocalUserChoices } from '@livekit/components-react';
+
+const LOCAL_STORAGE_KEY = 'livekit_meet';
+
+type LocalStorageData = {
+  userChoices: LocalUserChoices;
+  autoJoinRoom: boolean;
+};
+
+export function getLocalStorageData(): LocalStorageData | undefined {
+  if (typeof localStorage === 'undefined') {
+    return undefined;
+  }
+  const data = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (!data) {
+    return undefined;
+  }
+  return JSON.parse(data);
+}
+
+export function setLocalStorageData(data: LocalStorageData) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
+}
