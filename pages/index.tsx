66e4fcdd27fd48a298cd81acda8108a7630ef426
lukasIO <mail@lukasseiler.de>
2023-11-07 15:44:44 +0200

    Move e2ee options to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,8 +39,14 @@ function Tabs(props: TabsProps) {
 
 function DemoMeetingTab({ label }: { label: string }) {
   const router = useRouter();
+  const [e2ee, setE2ee] = useState(false);
+  const [sharedPassphrase, setSharedPassphrase] = useState(randomString(64));
   const startMeeting = () => {
-    router.push(`/rooms/${generateRoomId()}`);
+    if (e2ee) {
+      router.push(`/rooms/${generateRoomId()}#${encodePassphrase(sharedPassphrase)}`);
+    } else {
+      router.push(`/rooms/${generateRoomId()}`);
+    }
   };
   return (
     <div className={styles.tabContent}>
@@ -48,6 +54,28 @@ function DemoMeetingTab({ label }: { label: string }) {
       <button style={{ marginTop: '1rem' }} className="lk-button" onClick={startMeeting}>
         Start Meeting
       </button>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+        <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+          <input
+            id="use-e2ee"
+            type="checkbox"
+            checked={e2ee}
+            onChange={(ev) => setE2ee(ev.target.checked)}
+          ></input>
+          <label htmlFor="use-e2ee">Enable end-to-end encryption</label>
+        </div>
+        {e2ee && (
+          <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+            <label htmlFor="passphrase">Passphrase</label>
+            <input
+              id="passphrase"
+              type="password"
+              value={sharedPassphrase}
+              onChange={(ev) => setSharedPassphrase(ev.target.value)}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
