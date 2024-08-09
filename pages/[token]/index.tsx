982f21da9db4591870a6185942d0828d43c1cf2d
Michael Osoba <osoba.michael@outlook.com>
2024-08-10 00:31:09 +0100

    fx: add token page

---
+++ pages/[token]/index.tsx
@@ -0,0 +1,87 @@
+'use client';
+import { formatChatMessageLinks, LiveKitRoom, VideoConference } from '@livekit/components-react';
+import {
+  ExternalE2EEKeyProvider,
+  LogLevel,
+  Room,
+  RoomConnectOptions,
+  RoomOptions,
+  VideoCodec,
+  VideoPresets,
+} from 'livekit-client';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
+import { decodePassphrase } from '../../lib/client-utils';
+import { DebugMode } from '../../lib/Debug';
+
+export default function CustomRoomConnection() {
+  const router = useRouter();
+  const { token, codec } = router.query;
+  const liveKitUrl = process.env.LIVEKIT_URL || 'wss://anglequest-zyu8yj6j.livekit.cloud';
+
+  const e2eePassphrase =
+    typeof window !== 'undefined' && decodePassphrase(window.location.hash.substring(1));
+  const worker =
+    typeof window !== 'undefined' &&
+    new Worker(new URL('livekit-client/e2ee-worker', import.meta.url));
+  const keyProvider = new ExternalE2EEKeyProvider();
+
+  const e2eeEnabled = !!(e2eePassphrase && worker);
+
+  const roomOptions = useMemo((): RoomOptions => {
+    return {
+      publishDefaults: {
+        videoSimulcastLayers: [VideoPresets.h540, VideoPresets.h216],
+        red: !e2eeEnabled,
+        videoCodec: codec as VideoCodec | undefined,
+      },
+      adaptiveStream: { pixelDensity: 'screen' },
+      dynacast: true,
+      e2ee: e2eeEnabled
+        ? {
+            keyProvider,
+            worker,
+          }
+        : undefined,
+    };
+  }, []);
+
+  const room = useMemo(() => new Room(roomOptions), []);
+  if (e2eeEnabled) {
+    keyProvider.setKey(e2eePassphrase);
+    room.setE2EEEnabled(true);
+  }
+  const connectOptions = useMemo((): RoomConnectOptions => {
+    return {
+      autoSubscribe: true,
+    };
+  }, []);
+
+  console.log(typeof liveKitUrl)
+  console.log(typeof token)
+
+  if (typeof liveKitUrl !== 'string') {
+    return <h2>Missing meeting code</h2>;
+  }
+  if (typeof token !== 'string') {
+    return <h2>Missing meeting code</h2>;
+  }
+
+  return (
+    <main data-lk-theme="default">
+      {liveKitUrl && (
+        <LiveKitRoom
+          room={room}
+          token={token}
+          connectOptions={connectOptions}
+          serverUrl={liveKitUrl}
+          audio={true}
+          video={true}
+        >
+          <VideoConference chatMessageFormatter={formatChatMessageLinks} />
+          <DebugMode logLevel={LogLevel.debug} />
+        </LiveKitRoom>
+      )}
+    </main>
+  );
+}
