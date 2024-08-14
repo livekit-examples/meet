35d31c677127b6d25f386cc41deec36ede94d4df
Jonas Schell <jonas@livekit.io>
2024-08-14 23:26:24 +0200

    remove deprecated field
    from docs: @deprecated — will be enabled by default and removed in Next.js 15

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: false,
   productionBrowserSourceMaps: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
     // Important: return the modified config
