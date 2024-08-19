4857ea03b578c13f8ade4ba9a0457f0121f50c61
Jonas Schell <jonas@livekit.io>
2024-08-19 11:59:56 +0200

    simplify

--- lib/server-utils.ts
+++ lib/server-utils.ts
@@ -1,6 +1,6 @@
 export function getLiveKitURL(region: string | null): string {
   let targetKey = 'LIVEKIT_URL';
-  if (region && !Array.isArray(region)) {
+  if (region) {
     targetKey = `LIVEKIT_URL_${region}`.toUpperCase();
   }
   const url = process.env[targetKey];
