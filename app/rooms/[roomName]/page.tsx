9c375d37eb19628b2b5085d33e1943e86014a666
lukasIO <mail@lukasseiler.de>
2025-04-08 08:03:26 +0200

    fix code for v15 upgrade

--- app/rooms/[roomName]/page.tsx
+++ app/rooms/[roomName]/page.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { PageClientImpl } from './PageClientImpl';
 import { isVideoCodec } from '@/lib/types';
 
-export default function Page({
+export default async function Page({
   params,
   searchParams,
 }: {
-  params: { roomName: string };
+  params: Promise<{ roomName: string }>;
   searchParams: {
     // FIXME: We should not allow values for regions if in playground mode.
     region?: string;
@@ -14,13 +14,20 @@ export default function Page({
     codec?: string;
   };
 }) {
+  const _params = await params;
+  const _searchParams = await searchParams;
   const codec =
-    typeof searchParams.codec === 'string' && isVideoCodec(searchParams.codec)
-      ? searchParams.codec
+    typeof _searchParams.codec === 'string' && isVideoCodec(_searchParams.codec)
+      ? _searchParams.codec
       : 'vp9';
   const hq = searchParams.hq === 'true' ? true : false;
 
   return (
-    <PageClientImpl roomName={params.roomName} region={searchParams.region} hq={hq} codec={codec} />
+    <PageClientImpl
+      roomName={_params.roomName}
+      region={_searchParams.region}
+      hq={hq}
+      codec={codec}
+    />
   );
 }
