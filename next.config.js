beb7e5d9d13f1bb5ec19b0957819ead01470c269
lukasIO <mail@lukasseiler.de>
2025-04-15 13:00:49 +0200

    ugh

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
+  images: {
+    formats: ['image/webp'],
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
     // Important: return the modified config
     config.module.rules.push({
@@ -9,18 +12,7 @@ const nextConfig = {
       enforce: 'pre',
       use: ['source-map-loader'],
     });
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|mp4)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next',
-            name: 'static/media/[name].[hash].[ext]',
-          },
-        },
-      ],
-    });
+
     return config;
   },
 };
